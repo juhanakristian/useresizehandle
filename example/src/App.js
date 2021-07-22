3d84fdbd69b25ce401556f6e1fb3314d899dd0ac
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-07-22 23:10:19 +0300

    docs: added example

---
+++ example/src/App.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useResize } from "useresize";
+
+function App() {
+  const { containerProps, handleProps } = useResize({ axis: "both" });
+
+  return (
+    <div className="App">
+      <div {...containerProps}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div {...handleProps}>
+          <div
+            style={{ width: 30, height: 30, backgroundColor: "black" }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
