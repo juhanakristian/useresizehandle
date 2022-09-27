e070f4c7fb6fdbc85854ec82c41f1784163bebc2
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2022-09-27 18:57:02 +0300

    fix: React 18 support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import { useThrottle } from "./util";
-
 interface UseResizeHandleConfig {
   axis?: "horizontal" | "vertical" | "both";
   onResizeStart?: () => void;
